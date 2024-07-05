c81db482e6d51d1d4b4146307d2eabc4f64d2f6c
慕容君琳 <147541477@qq.com>
2024-07-05 15:59:34 +0800

    Add files via upload
    学习注释的添加

--- c语言日常学习小练习text.c
+++ c语言日常学习小练习text.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+//预处理
+ #include<stdio.h>
+//主入口
 int main()
 {
+	//输出语句，小括号里面表示输出的内容
 printf("heiio word");
+/*return：程序结束
+0表示正常结束
+这里的0，需要和前面int相呼应
+CTRL F5是调试快捷键*/
 return 0;
 }
