1f17b4d263f96b626a0043683eff01a76d821256
慕容君琳 <147541477@qq.com>
2024-07-05 17:14:14 +0800

    Add files via upload
    常数的输入

---
+++ text2.c
@@ -0,0 +1,21 @@
+//预处理
+#include<stdio.h>
+//主入口
+int main()
+{//输出常量
+	//输出整数
+	//printf("%d",18);
+
+	//输出小数
+	//printf("%f", 18.5);
+	
+	//输入字符
+	//printf("%c", 'A');
+
+	//输出字符串
+	//printf("hello word");
+
+	//第二种方式
+	//printf("我的名字是:%s","慕容君琳");
+	return 0;
+}
