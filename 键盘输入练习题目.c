43681beb719c5331dcf36178439d0cba939bb9b6
慕容君琳 <147541477@qq.com>
2024-07-10 15:02:53 +0800

    Add files via upload

---
+++ 键盘输入练习题目.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+int main()
+{
+	char name[100];
+	printf("请输入你名字：\n" );
+	scanf("%s", &name);
+
+	printf("我名字是：%s\n",name);
+
+
+	int age ;
+	printf("请输入你年龄：\n");
+	scanf("%d", &age);
+    printf("我名字是年龄：%d岁\n", age);
+
+	//输入并求和
+
+	int num1;
+	int num2;
+	scanf("%d",&num1);
+	scanf("%d", &num2);
+	printf("%d\n", num1 + num2);
+
+
+	
+
+
+
+	return 0;
+}
