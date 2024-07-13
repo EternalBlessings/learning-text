1965b5dedf2ee1478504b6b14d7fe2e092b33630
慕容君琳 <147541477@qq.com>
2024-07-13 15:12:36 +0800

    Add files via upload

---
+++ 数字拆分.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS  
+#include<stdio.h>  
+
+/*int main()
+{
+     //需求在键盘录入一个三位数，将其拆分为个十百，例如123 
+    printf("请输入一个三位数:\n");
+    int num;
+    scanf("%d", &num);
+    printf("%d\n", num);
+    // 拆分  
+    int ge = num % 10;
+    int shi = num / 10 % 10;
+    int bai = num / 100;
+    // 输出  
+    printf("个位：%d\n", ge);
+    printf("十位：%d\n", shi);
+    printf("百位：%d\n", bai);
+    return 0;
+}*/
