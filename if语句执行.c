b9f9cf93bd514e29dfa90075a9f9aad822673b96
慕容君琳 <147541477@qq.com>
2024-07-20 16:16:45 +0800

    Add files via upload

---
+++ if语句执行.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>  
+
+int main()
+{//任务：角色血量最大200，受到伤害80，恢复血量100，请问现在最大多少
+  /*  int blood=200;
+    int hurt=80;
+    int restore=100;
+    blood = blood - hurt + restore;
+    if (blood >= 200);
+    {
+        blood = 200;
+    }
+    printf("%d\n", blood);*/
+
+
+    //if else二选一使用：
+    //任务：相亲时候如果女生对男生满意则说 终身大事全凭父母做主
+  //否则就说女儿还想孝敬父母
+    int satisfy = 1;
+    if (satisfy)
+    {
+        printf("终身大事全凭父母做主");
+    }
+    else
+    {
+        printf("女儿还想孝敬父母");
+    }
+    return 0;
+
+}
